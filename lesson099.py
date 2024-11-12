179c3c710b56b44b3f6534e263a470d574e68328
Mark Peters <mapeters101@earthlink.net>
2024-11-12 14:03:38 -0500

    Lesson099:  Work on lesson from 5:10 to 10:20.

--- lesson099.py
+++ lesson099.py
@@ -41,6 +41,41 @@ print(my_func3(2))
 print(f'main: {a=}')
 print()
 
+def my_func4():
+    global var
+    var = 'hello world'
+    return
+
+#print(var)
+my_func4()
+print(var)
+print()
+
+
+a = 10
+def my_func5():
+    global a
+    a = 'hello'
+    print('global a:', a)
+    return
+
+print(f'{a=}')
+my_func5()
+print(a)
+print()
+
+# a = 10
+# def my_func6():
+#     print('global a:', a)
+#     a = 'hello world'
+#     print(a)
+#     return
+#
+# print(f'{a=}')
+# my_func6()
+# print(a)
+# print()
+
 
 
 
