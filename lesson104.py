c61acc62827e6766671b947daaae62a68e24e3ab
Mark Peters <mapeters101@earthlink.net>
2024-11-12 17:10:36 -0500

    Lesson104:  Work on lesson to 5:00.

---
+++ lesson104.py
@@ -0,0 +1,52 @@
+class Averager:
+    def __init__(self):
+        self.numbers = []
+
+    def add(self, number):
+        self.numbers.append(number)
+        total = sum(self.numbers)
+        count = len(self.numbers)
+        return total / count
+
+a = Averager()
+print(a.add(10))
+print(a.add(20))
+print(a.add(30))
+print()
+
+b = Averager()
+print(b.add(10))
+print(b.add(20))
+print(b.add(30))
+print('='*40)
+
+def averager():
+    numbers = []
+    def add(number):
+        numbers.append(number)
+        total = sum(numbers)
+        count = len(numbers)
+        return total / count
+    return add
+
+a = averager()
+print(a(10))
+print(a(20))
+print(a(30))
+print()
+
+b = averager()
+print(b(10))
+print(b(20))
+print(b(30))
+print()
+
+
+
+
+
+
+
+
+
+
