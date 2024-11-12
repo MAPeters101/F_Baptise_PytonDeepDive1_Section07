a1eb190997c7469ba598b8ce6eee9225666938e0
Mark Peters <mapeters101@earthlink.net>
2024-11-12 14:52:03 -0500

    Lesson101:  Complete work on lesson from 10:00.

--- lesson101.py
+++ lesson101.py
@@ -61,7 +61,68 @@ outer_func5()
 #print('outer(after outer):', x)
 print()
 
+def outer_func6():
+    x = 'hello6'
+    def inner1():
+        nonlocal x
+        x = 'python'
+        def inner2():
+            nonlocal x
+            x = 'monty'
+            print('inner:', x)
+        print('outer(before inner2):', x)
+        inner2()
+    print('outer(before inner1):', x)
+    inner1()
+    print('outer(after inner1):', x)
 
+outer_func6()
+#print('outer(after outer):', x)
+print('='*30)
+
+# x = 'python'
+# def outer_func7():
+#     global x
+#     x = 'monty7'
+#     def inner():
+#         nonlocal x
+#         x = 'hello7'
+#     print('outer(before inner):', x)
+#     inner()
+#     print('outer(after inner):', x)
+#
+# outer_func7()
+# print()
+
+x = 'python'
+def outer_func8():
+    x = 'monty8'
+    def inner():
+        nonlocal x
+        x = 'hello8'
+    print('outer(before inner):', x)
+    inner()
+    print('outer(after inner):', x)
+
+outer_func8()
+print(x)
+print()
+
+
+x = 'python'
+def outer_func8():
+    global x
+    x = 'monty8'
+    def inner():
+        global x
+        x = 'hello8'
+    print('outer(before inner):', x)
+    inner()
+    print('outer(after inner):', x)
+
+outer_func8()
+print(x)
+print()
 
 
 
